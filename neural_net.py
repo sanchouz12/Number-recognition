19d5fce86a1ff7bb6080cb85548c46dedf7ce8e3
Sanchouz <govnocoder99@gmail.com>
2020-02-11 19:42:49 +0200

    Finished upgrading the code

--- neural_net.py
+++ neural_net.py
@@ -46,13 +46,14 @@ class NeuralNet:
         input_arr = numpy.array(input_list, ndmin = 2).T
         target_arr = numpy.array(target_list, ndmin = 2).T
 
-        self.outputs.append(input_arr)
-
         self.query(input_arr)
 
         self.count_errors(target_arr)
 
-        self.update_weights(input_arr)
+        self.update_weights()
+
+        self.outputs = []
+        self.errors = []
 
     def query(self, input_arr):
         """
@@ -61,15 +62,17 @@ class NeuralNet:
         :param input_arr: numpy array of inputs for a neural net
         :return final_outputs: numpy array of outputs of the output layer
         """
+        self.outputs.append(input_arr)
+
         first_hidden_inputs = numpy.dot(self.weights[0], input_arr)
         first_hidden_outputs = self.activation(first_hidden_inputs)
 
         self.outputs.append(first_hidden_outputs)
 
         # last is not included
-        for index in range(1, len(self.weights) - 1):
-            weights_arr = self.weights[index]
-            prev_outputs = self.outputs[index]
+        for index in range(2, len(self.weights)):
+            weights_arr = self.weights[index - 1]
+            prev_outputs = self.outputs[index - 1]
 
             curr_inputs = numpy.dot(weights_arr, prev_outputs)
             curr_outputs = self.activation(curr_inputs)
@@ -100,23 +103,23 @@ class NeuralNet:
         :param target_arr: numpy array of expected values
         """
         self.errors.append(target_arr - self.outputs[-1])
-        for index in range(len(self.outputs) - 2, -1, -1):
-            self.errors.insert(0, numpy.dot(self.weights[index].T, self.errors[0]))
+        for index in range(len(self.weights) - 2, -1, -1):
+            prev_errors = self.errors[0]
 
-    def update_weights(self, input_arr):
+            self.errors.insert(0, numpy.dot(self.weights[index + 1].T, prev_errors))
+
+    def update_weights(self):
         """
         This function updates all weights.
-
-        :param input_arr: numpy array of inputs for a neural net
         """
         self.weights[0] += self.learning_rate * numpy.dot(
-            (self.errors[0] * self.outputs[0] * (1.0 - self.outputs[0])),
-            numpy.transpose(input_arr)
+            (self.errors[0] * self.outputs[1] * (1.0 - self.outputs[1])),
+            numpy.transpose(self.outputs[0])
         )
         for index in range(1, len(self.weights)):
-            curr_errors = self.errors[0][index - 1]
-            curr_output = self.outputs[index]
-            prev_output = self.outputs[index - 1]
+            curr_errors = self.errors[index]
+            curr_output = self.outputs[index + 1]
+            prev_output = self.outputs[index]
 
             self.weights[index] += self.learning_rate * numpy.dot(
                 (curr_errors * curr_output * (1.0 - curr_output)),
